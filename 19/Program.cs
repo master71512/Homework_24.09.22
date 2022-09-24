fea18954677a564a370ba3e6a54e46ea053abca5
dmitriy <master71512@mail.ru>
2022-09-24 13:01:32 +0300

    19 solved

---
+++ 19/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 5-значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num<10000) | (num > 99999)) Console.WriteLine("число не 5-значное");
+else
+{
+    if((num / 10000 == num % 10) & (num / 1000 % 10 == num % 100 / 10)) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
