2b08b7d4fada6d4cb7cfc57c86cbaf4484c163d7
dmitriy <master71512@mail.ru>
2022-09-24 19:36:07 +0300

    21 solved

---
+++ 21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("ВВедите координаты двух точек в пространстве");
+Console.Write("x1= ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1= ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1= ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("x2= ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2= ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2= ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) +(z1 - z2)*(z1 - z2));
+result = Math.Round(result, 2);
+Console.WriteLine(result);
